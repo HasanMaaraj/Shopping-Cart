0e2336c1fd68860602b69eff655819862980a78f
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-02-14 15:20:19 +0300

    Configure website routing

---
+++ src/Router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import ErrorPage from "./ErrorPage";
+import Shop from "./Shop";
+import Product from "./Product";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+    },
+    {
+        path: "shop",
+        element: <Shop />
+    },
+    {
+        path: "product/:name",
+        element: <Product />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
