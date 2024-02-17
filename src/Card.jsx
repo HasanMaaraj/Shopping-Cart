5e61e81ea3069379af3f2015e51778d609fdc119
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-02-17 14:55:33 +0300

    Link the cards in shop to product pages

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom"
 const Card = ({ product }) => {
     return (
         <div className="card">
+            <Link to={`/product/${product.id}`} >
                     <div>{product.title}</div>
                     <div>{product.category}</div>
                     <div>{product.description}</div>
                     <img src={product.image} alt={product.title} />
+            </Link>
+                    <input type="number" />
+                    <button>Add to Cart</button>
         </div>
     )
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
+    const { id } = useParams();
+    const [product, setProduct] = useState(null)
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(response=> {
+            if (!response.ok) throw new Error("Server error");
+            return response.json()
+        })
+        .then(data => setProduct(data))
+        .catch(error => setError(error))
+        .finally(() => setLoading(false))
+
+    if (error) return <p>A network error was encountered</p>;
+    if (loading) return <p>Loading...</p>;
+
+    return (
+        <div className="product">
+                <div>{product.title}</div>
+                <div>{product.category}</div>
+                <div>{product.description}</div>
+                <img src={product.image} alt={product.title} />
+                <input type="number" />
+                <button>Add to Cart</button>
+    </div>
+    )
 };
 
 export default Product;
