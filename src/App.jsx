1b2eb15a4102fb912393e4e1136ab01d54c46d1d
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-02-18 19:07:24 +0300

    Delete unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Link } from 'react-router-dom'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,4 +12,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
