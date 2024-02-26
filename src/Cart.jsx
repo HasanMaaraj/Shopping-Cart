dd4679c267c06a8c2c46402e54eb74ca6c37951e
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-02-26 21:26:02 +0300

    Display cart items on cart page

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -3,49 +3,39 @@ import { useContext, useEffect, useState } from "react";
 import Header from "./Header";
 import Loading from "./Loading";
 import NetworkError from "./NetworkError";
-
+import { v4 as uuidv4 } from 'uuid';
 const Cart = () => {
     const {cart, setCart} = useContext(CartContext);
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
-    const urls = []
-
-    Object.keys(cart).forEach(product => {
-        urls.push(`https://fakestoreapi.com/products/${product}`);
-    })
-
     useEffect(() => {
-        urls.forEach(url => {
-            fetch(url)
-                .then(response => {
-                    if (!response.ok) throw new Error("Server error");
-                    return response.json()
-                })
-                .then(data => setProducts([...products, data]))
-        })
-    }, [])
+    
+        fetch('https://fakestoreapi.com/products')
+            .then(response => response.json())
+            .then(data => setProducts(data.filter(product => Object.keys(cart).includes(String(product.id)))))
+            .catch(error => setError(error))
+            .finally(() => setLoading(false))
 
-    useEffect(() => {
-        if (products.length === Object.keys(cart).length) setLoading(false)
-    }, [products, cart])
+    }, [cart])
 
-    if (error) return <NetworkError />;
-    if (loading) return <Loading />;
+
+        if (loading) return <Loading />;
+        if (error) return <NetworkError />;
 
     return (
         <div className="container">
         <Header />
             <main>
-
-            {products.length > 1 ? products.map(product => {
-                return (<>
+            {Object.keys(products).length}
+            {Object.keys(cart).length > 0 ? products.map(product => {
+                return (<div className="cart-product" key={uuidv4()}>
             <div className="product-title">{product.title}</div>
             <div className="product-category">{product.category}</div>
             <div className="product-description">{product.description}</div>
             <img src={product.image} alt={product.title} className="product-image"/>
-            </>)
+            </div>)
         }): <h2>Empty Cart</h2>}
             </main>
         </div>
