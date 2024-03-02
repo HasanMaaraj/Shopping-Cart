5ae74549ab7265bd66a4c9467d7d4ae31a5e2051
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-03-02 18:44:35 +0300

    Add product to cart using ref instead of DOM manipulation

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import CartContext from "./CartContext";
 import Header from "./Header";
@@ -11,7 +11,7 @@ const Product = () => {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
     const {cart, setCart} = useContext(CartContext)
-
+    const productValueRef = useRef(null)
     fetch(`https://fakestoreapi.com/products/${id}`)
         .then(response=> {
             if (!response.ok) throw new Error("Server error");
@@ -28,22 +28,22 @@ const Product = () => {
         <div className="container">
             <Header />
             <main>
-
                 <div className="product">
-
                     <div className="product-title">{product.title}</div>
                     <div className="product-category">{product.category}</div>
                     <div className="product-description">{product.description}</div>
                     <img src={product.image} alt={product.title} className="product-image"/>
-                    <input type="number" id={product.id}/>
+                    <input type="number" id={product.id} min="0" ref={productValueRef}/>
                     <button onClick={ () => {
-                        const placedItems = parseInt(document.querySelector(`input[id="${product.id}"]`).value);
-                        setCart(() => {
-                            return {
-                                ...cart,
-                                [product.id]: cart[product.id] ? cart[product.id]+placedItems : placedItems
-                            }
-                        })
+                        const placedItems = parseInt(productValueRef.current.value);
+                        if (placedItems > 0) {
+                            setCart(() => {
+                                return {
+                                    ...cart,
+                                    [product.id]: cart[product.id] ? cart[product.id]+placedItems : placedItems
+                                }
+                            })
+                        }
                     } }>Add to Cart</button>
                 </div>
             </main>
