b16f7d56f8b25eff97220dc2a6344c62b3a5ed3c
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-03-02 16:15:30 +0300

    Move each cart card to a separate component

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -4,6 +4,8 @@ import Header from "./Header";
 import Loading from "./Loading";
 import NetworkError from "./NetworkError";
 import { v4 as uuidv4 } from 'uuid';
+import CartCard from "./CartCard";
+
 const Cart = () => {
     const {cart, setCart} = useContext(CartContext);
     const [products, setProducts] = useState([]);
@@ -30,33 +32,8 @@ const Cart = () => {
             <main>
             {Object.keys(cart).length > 0 ? <div className="cart">
             {products.map(product => {
-                return (<div className="cart-item" id={`cart-item-${product.id}`}  key={uuidv4()}>
-            <div className="product-title"><h2>{product.title}</h2></div>
-            <div className="product-category"><h3>{product.category}</h3></div>
-            <div className="product-description">{product.description}</div>
-            <img src={product.image} alt={product.title} className="product-image"/>
-            <div className="prices">
-                <div className="item-price">Price: ${product.price}</div>
-                <div className="item-total-price">Total: ${product.price*cart[product.id]}</div>
-            </div>
-            <div className="edit-item">
-
-            <input type="number" id={`cart-${product.id}-value`} defaultValue={cart[product.id]}/>
-            <button onClick={() => {
-                setCart({
-                    ...cart,
-                    [product.id]: parseInt(document.getElementById(`cart-${product.id}-value`).value),
-                })
-            }}>Change Item Quantity</button>
-
-            <button className="remove-btn" onClick={() => {
-                const newCart = {...cart}
-                delete newCart[product.id]
-                setCart(newCart)
-            }}>Remove From Cart</button>
-            </div>
-            </div>)
-        }
+                return <CartCard product={product} key={uuidv4()} />
+            }
         )}
         
         <div className="payment">
---
+++ src/CartCard.jsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import CartContext from "./CartContext";
+
+const CartCard = ({ product }) => {
+    const {cart, setCart} = useContext(CartContext)
+    return (
+    <div className="cart-item" id={`cart-item-${product.id}`}>
+        <div className="product-title"><h2>{product.title}</h2></div>
+        <div className="product-category"><h3>{product.category}</h3></div>
+        <div className="product-description">{product.description}</div>
+        <img src={product.image} alt={product.title} className="product-image"/>
+        <div className="prices">
+            <div className="item-price">Price: ${product.price}</div>
+            <div className="item-total-price">Total: ${product.price*cart[product.id]}</div>
+        </div>
+        <div className="edit-item">
+
+        <input type="number" id={`cart-${product.id}-value`} defaultValue={cart[product.id]}/>
+        <button onClick={() => {
+            setCart({
+                ...cart,
+                [product.id]: parseInt(document.getElementById(`cart-${product.id}-value`).value),
+            })
+        }}>Change Item Quantity</button>
+
+        <button className="remove-btn" onClick={() => {
+            const newCart = {...cart}
+            delete newCart[product.id]
+            setCart(newCart)
+        }}>Remove From Cart</button>
+        </div>
+    </div>)
+}
+
+export default CartCard;
+
